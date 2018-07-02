1ff1a45e9d32d540ffa3ecebed4b80c028d72f0d
VINT64 <vint64@mail.com>
2018-07-02 16:47:23 +0200

    adding params, cross-origin

--- src/main/java/com/dataviz/DemoApplication.java
+++ src/main/java/com/dataviz/DemoApplication.java
@@ -2,6 +2,8 @@ package com.dataviz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.*;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -9,4 +11,15 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/services/dummyRequest").allowedOrigins("*");
+			}
+		};
+	}
 }
+
--- src/main/java/com/dataviz/ServiceController.java
+++ src/main/java/com/dataviz/ServiceController.java
@@ -1,13 +1,53 @@
 package com.dataviz;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.util.*;
 
 @Controller
 public class ServiceController {
-    @GetMapping("/services/dummyRequest")
-    public ModelAndView success(){
-        return new ModelAndView("test");
+    private static final String quandlTemplate = "https://www.quandl.com/api/v3/datasets/%s/%s.json";
+    private static final String apiKey = "DKczFdjuL_16KZVxeZKk";
+
+    @RequestMapping(value="/services/dummyRequest", method = RequestMethod.GET, produces = "application/json")
+     public @ResponseBody String success(
+   /* public ModelAndView success( */
+        @RequestParam(name="database_code", required=true) String databaseCode,
+        @RequestParam(name="dataset_code", required=true) String datasetCode,
+        @RequestParam(name="start_date", required=true) String startDate,
+        @RequestParam(name="end_date", required=true) String endDate,
+        @RequestParam(name="order", required=true) String order,
+        @RequestParam(name="collapse", required=true) String collapse,
+        @RequestParam(name="transformation", required=true) String transformation,
+        @RequestParam(name="limit", defaultValue="0") String limit,
+        @RequestParam(name="column_index", defaultValue ="-1") String columnIndex
+        ){
+        RestTemplate restTemplate = new RestTemplate();
+        Map<String, String> params = new HashMap<>();
+        params.put("api_key", apiKey);
+        params.put("transformation", transformation);
+        params.put("collapse", collapse);
+        params.put("order", order);
+        params.put("end_date", endDate);
+        params.put("start_date", startDate);
+        if(Integer.valueOf(limit)>0){
+            params.put("limit", limit);
+        }
+        if(Integer.valueOf(columnIndex)>-1){
+            params.put("column_index", columnIndex);
+        }
+        String url = String.format(quandlTemplate, databaseCode, datasetCode) ;
+        String result = null;
+        try {
+            result = restTemplate.getForObject(url, String.class, params);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+        /*return new ModelAndView("test");*/
     }
 }
