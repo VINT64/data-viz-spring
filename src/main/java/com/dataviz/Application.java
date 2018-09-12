7b4515e8cdcd1e79bdbdfc77c075a45c5553895f
VINT64 <vint64@mail.com>
2018-09-12 15:39:17 +0200

    fixes

--- src/main/java/com/dataviz/Application.java
+++ src/main/java/com/dataviz/Application.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.*;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 	@Bean
